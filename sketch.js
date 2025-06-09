1e82bf118f242ee08ac451bca116fe0a2b23c9da
sir-cakes-alot <pleasework413@gmail.com>
2025-06-08 22:05:02 -0800

    Update sketch.js
    added moving circle

--- sketch.js
+++ sketch.js
@@ -2,8 +2,10 @@ function setup() {
   createCanvas(400, 400);
   background(0);
 }
+var test = 0;
 
 function draw() {
   fill(255);
-  ellipse(width/2, height/2, 100, 100);
+  test = test>60 ? 0 :test+1;
+  ellipse(test+(width/2), (height/2)-test, 100, 100);
 }

9cb4b91d7c2858dd6ca9c250eeb76c3f1fe22bb0
sir-cakes-alot <pleasework413@gmail.com>
2025-06-09 21:28:14 -0800

    Update sketch.js
    circle motion

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
+let x = 0;
+let y = 0;
+let speed = 5;
+
 function setup() {
   createCanvas(1000, 400);
-  background(0);
 }
-var test = 0;
 
 function draw() {
+  background(0);
   fill(255);
-  test = test>100 ? 0 :test+1;
-  ellipse(test+(width/2), (height/2)-test, 100, 100);
+  ellipse(x + width / 2, height / 2 - y, 100, 100);
+
+  // Handle movement
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) { // A
+    x -= speed;
+  }
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) { // D
+    x += speed;
+  }
+  if (keyIsDown(UP_ARROW) || keyIsDown(87)) { // W
+    y += speed;
+  }
+  if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) { // S
+    y -= speed;
+  }
 }

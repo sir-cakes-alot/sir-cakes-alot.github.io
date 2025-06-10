46efc2e83fbd4829e72dfe0f0980b2e7dbf9b638
sir-cakes-alot <pleasework413@gmail.com>
2025-06-09 22:20:50 -0800

    Update sketch.js
    wap a bap

--- sketch.js
+++ sketch.js
@@ -1,27 +1,47 @@
-let x = 0;
-let y = 0;
-let speed = 5;
+let pos, vel, targetVel;
+let accel = 0.2;
+let maxSpeed = 8;
 
 function setup() {
   createCanvas(1000, 400);
+  pos = createVector(width / 2, height / 2);
+  vel = createVector(0, 0);
+  targetVel = createVector(0, 0);
 }
 
 function draw() {
   background(0);
   fill(255);
-  ellipse(x + width / 2, height / 2 - y, 100, 100);
+  ellipse(pos.x, pos.y, 100, 100);
 
-  // Handle movement
-  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) { // A
-    x -= speed;
+  // Movement input: WASD + arrow keys
+  targetVel.set(0, 0);
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+    targetVel.x = -maxSpeed;
   }
-  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) { // D
-    x += speed;
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
+    targetVel.x = maxSpeed;
   }
-  if (keyIsDown(UP_ARROW) || keyIsDown(87)) { // W
-    y += speed;
+  if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
+    targetVel.y = -maxSpeed;
   }
-  if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) { // S
-    y -= speed;
+  if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
+    targetVel.y = maxSpeed;
+  }
+
+  // Smooth velocity change
+  vel.lerp(targetVel, accel);
+
+  // Move circle
+  pos.add(vel);
+
+  // Bounce off walls
+  if (pos.x <= 50 || pos.x >= width - 50) {
+    vel.x *= -1;
+    pos.x = constrain(pos.x, 50, width - 50);
+  }
+  if (pos.y <= 50 || pos.y >= height - 50) {
+    vel.y *= -1;
+    pos.y = constrain(pos.y, 50, height - 50);
   }
 }
